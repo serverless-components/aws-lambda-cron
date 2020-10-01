6f9bd09f0045c92a926f6881e8d6e96b9c46634a
Dwaipayan Ray <dwaipayanray1@gmail.com>
2020-10-02 01:30:34 +0530

    Feature: Regex matching to validate schedule expression

--- src/serverless.js
+++ src/serverless.js
@@ -7,17 +7,39 @@ const fs = require("fs");
 const { createOrUpdateMetaRole } = require("./utils");
 
 class LambdaCron extends Component {
-  async deploy(inputs = {}) {
-    if (Object.keys(this.credentials.aws).length === 0) {
-      const msg = `Credentials not found. Make sure you have a .env file in the cwd. - Docs: https://git.io/JvArp`;
-      throw new Error(msg);
-    }
-
+  validate(inputs) {
     if (!inputs.schedule) {
       throw new Error(
         "Input 'schedule' is required. Please see README: https://git.io/JJWW0"
       );
     }
+    const { schedule }=inputs.schedule
+
+    let valid = false
+
+    // Check for a cron expression
+    let pat = /(@(annually|yearly|monthly|weekly|daily|hourly|reboot))|(@every (\d+(ns|us|µs|ms|s|m|h))+)|((((\d+,)+\d+|(\d+(\/|-)\d+)|\d+|\*) ?){5,7})/
+    if (pat.test(schedule)) {
+      valid = true;
+    }
+
+    // Check for a rate expression
+    pat = /rate(\d+\s+(minute|minutes|hour|hours|day|days))/
+    if (pat.test(schedule)) {
+      valid = true
+    }
+
+    if(!valid) {
+      console.error("Schedule string is possibly wrong. Please recheck it.")
+    }
+  }
+  async deploy(inputs = {}) {
+    if (Object.keys(this.credentials.aws).length === 0) {
+      const msg = `Credentials not found. Make sure you have a .env file in the cwd. - Docs: https://git.io/JvArp`;
+      throw new Error(msg);
+    }
+    this.validate(inputs)
+
     const region = inputs.region || "us-east-1";
     inputs.name = inputs.name || this.name;
 
